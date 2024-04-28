1c1ac400278b7e87fc3ca862abcda1f93006e554
marijnnl32 <marijn@netje.nl>
2024-04-28 15:41:08 +0200

    Pokeflute gemaakt

--- src/main/java/firstplugin/firstplugin/Firstplugin.java
+++ src/main/java/firstplugin/firstplugin/Firstplugin.java
@@ -2,6 +2,7 @@ package firstplugin.firstplugin;
 
 import LuunkieKaasheer.LuunkieCheck; // Add this line
 import LuunkieKaasheer.LuunkieMoord;
+import firstplugin.firstplugin.Item.Pokeflute;
 import firstplugin.firstplugin.commands.Menu;
 import firstplugin.firstplugin.commands.vlieg;
 import firstplugin.firstplugin.handelers.Playerhandeler;
@@ -18,24 +19,24 @@ public final class Firstplugin extends JavaPlugin implements Listener {
 
     public static String kaasGod = "Keremyy";
 
-
-
     @Override
     public void onEnable() {
         // Plugin startup logic
         Bukkit.getLogger().info("goedemorgen");
 
+        // Initialize your items
+        Itemmanager.init();
+
+        // Register event listeners
         getServer().getPluginManager().registerEvents(new LuunkieCheck(), this);
+        getServer().getPluginManager().registerEvents(new Pokeflute(), this);
         getServer().getPluginManager().registerEvents(this, this);
 
-
-
-
-
+        // Initialize your handlers
         new Torchhandeler(this);
         new Playerhandeler(this);
 
-
+        // Register commands
         getCommand("vlieg").setExecutor(new vlieg());
         getCommand("menu").setExecutor(new Menu());
     }
@@ -46,7 +47,18 @@ public final class Firstplugin extends JavaPlugin implements Listener {
         Bukkit.getLogger().info("opgerot");
     }
 
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        // Your code for handling player death event
+    }
 
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        // Your code for handling player interact event
+    }
 
-
+    @EventHandler
+    public void onPlayerToggleSneak(PlayerToggleSneakEvent event) {
+        // Your code for handling player toggle sneak event
+    }
 }
---
+++ src/main/java/firstplugin/firstplugin/Item/Pokeflute.java
@@ -0,0 +1,54 @@
+package firstplugin.firstplugin.Item;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Pokeflute implements Listener {
+
+
+    public static void init() {
+        maakFluit();
+    }
+
+
+    private static void maakFluit(){
+        ItemStack pokeflute = new ItemStack(Material.PAPER, 1);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName("Pokeflute");
+        List<String> lore = new ArrayList<>();
+        lore.add("Use this to wake up sleeping pokemon");
+        meta.setLore(lore);
+        pokeflute.setItemMeta(meta);
+        pokeflute = pokeflute;
+
+
+
+
+    }
+
+
+
+    @EventHandler
+    public void useFlute(PlayerInteractEntityEvent event) {
+        Player player = event.getPlayer();
+        ItemStack item = player.getInventory().getItemInMainHand(); // Getting the item the player is holding
+
+        if (item.getType() == Material.PAPER && event.getRightClicked() instanceof Player) {
+            Player clickedPlayer = (Player) event.getRightClicked();
+            player.sendMessage("You have used the pokeflute.");
+            clickedPlayer.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 2400, 4));
+            clickedPlayer.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 600, 4));
+        }
+    }
+}
