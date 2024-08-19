61dff6205cadb2f8bda487fb01cab9c328cef081
Marijn Hazeveld <34924077+marijnnl32@users.noreply.github.com>
2024-08-19 14:24:16 +0200

    Update Firstplugin.java

--- src/main/java/firstplugin/firstplugin/Firstplugin.java
+++ src/main/java/firstplugin/firstplugin/Firstplugin.java
@@ -19,7 +19,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Firstplugin extends JavaPlugin implements Listener {
 
-    public static String kaasGod = "Keremyy";
+    public static String kaasGod = "Luuk7777";
 
     @Override
     public void onEnable() {
